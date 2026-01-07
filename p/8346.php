e14e7cfe98beb2a75f216239b7414381ba50100e
andykimpe1 <andykimpe@gmail.com>
2026-01-07 18:08:04 +0100

    Create 8346.php

---
+++ p/8346.php
@@ -0,0 +1,127 @@
+<article id="post-8346" class="post-8346 post type-post status-publish format-standard has-post-thumbnail hentry category-pokemon-horizons category-release">
+	<header class="entry-header entry-header-single">
+		<h1 class="entry-title">Pokémon Horizons 121 et 122 VOSTFR</h1>		<div class="entry-meta entry-meta-1"><span class="author vcard"><a class="url fn n" href="https://pokemonfansub.alwaysdata.net/?author=2">Trizek</a></span><span class="entry-date"><time class="entry-date published" datetime="2026-01-06T01:28:54+00:00">6 janvier 2026</time></span><span class="comments-link"><i class="fa fa-comments-o"></i><a href="https://pokemonfansub.alwaysdata.net/?p=8346#respond">0</a></span></div>	</header><!-- .entry-header -->
+
+
+	<div class="entry-content">
+		<p>Bonjour à tous !</p>
+<p>Friede a refait surface ! Il a contacté les autres Électacleurs Volants pour les prévenir de son retour&#8230; depuis l&rsquo;espace ? Mais que faisait-il là-haut ?</p>
+<p>Bonne année à tous, et bon visionnage !</p>
+<!--<p><img class="" src="https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ121.png" alt="" width="686" height="385" /></p>-->
+<p>PMHZ 121, Pokémon en français :
+
+
+<!--<a href="https://mega.nz/file/rUVhUbwQ#OiBxZZ5nuTiL2Hdd4CJEEKhF4y_YGMJtZETnqopAYvE">Mega</a>-->
+<a href="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-121-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+
+
+
+
+et <a href="https://nyaa.si/view/2060790">Torrent</a><br />
+
+
+<iframe width="640" height="500" frameborder="0" src="https://pokemonfansub.alwaysdata.net/video.php?link=https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-121-VOSTFR-FR-1920x1080-H264-AAC.mp4.php&poster=https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ121.png" allowfullscreen></iframe><br />
+
+
+
+PMHZ 121, Pokémon en japonais :
+
+
+<!--<a href="https://mega.nz/file/eNEglbBC#zK9Jxbf0Hyp0BT5dr--I4hYj2V51jXknOQGRinFsp04">Mega </a>-->
+<a href="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-121-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+
+
+
+
+
+
+
+et <a href="https://nyaa.si/view/2060796">Torrent</a></p>
+
+
+<br />
+
+
+
+<iframe width="640" height="500" frameborder="0" src="https://pokemonfansub.alwaysdata.net/video.php?link=https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-121-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php&poster=https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ121.png" allowfullscreen></iframe><br />
+
+
+
+
+
+<!--<p><img class="" src="https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ122.png" alt="" width="686" height="385" /></p>-->
+
+
+
+<br />
+
+<p>PMHZ 122, Pokémon en français :
+
+
+
+
+<!--<a href="https://mega.nz/file/qUlHGKQY#ods8VsLYwLl4RY6tPxvaVAh7DK4wbrePPFRsQ-T9DGw">Mega</a>-->
+<a href="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-122-VOSTFR-FR-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+
+
+
+
+
+et <a href="https://nyaa.si/view/2060792">Torrent</a><br />
+
+
+
+
+<iframe width="640" height="500" frameborder="0" src="https://pokemonfansub.alwaysdata.net/video.php?link=https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-122-VOSTFR-FR-1920x1080-H264-AAC.mp4.php&poster=https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ122.png" allowfullscreen></iframe><br />
+
+
+
+
+
+PMHZ 122, Pokémon en japonais :
+
+
+
+<!--<a href="https://mega.nz/file/HEsTQCoS#CzUJsk9M8r2amCbDmx5OfyNPTH50b4y1m0C9JzQ7sOM">Mega</a>-->
+<a href="https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-122-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php">Téléchargement direct</a>
+
+
+
+
+
+
+et <a href="https://nyaa.si/view/2060797">Torrent</a></p>
+
+
+
+<iframe width="640" height="500" frameborder="0" src="https://pokemonfansub.alwaysdata.net/video.php?link=https://pokemonfansub.alwaysdata.net/download/Pokemon-Fansub-Pokemon-Horizons-122-VOSTFR-JAP-1920x1080-H264-AAC.mp4.php&poster=https://pokemonfansub.alwaysdata.net/wp-content/uploads/HZ122.png" allowfullscreen></iframe><br />
+
+
+
+			</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<div class="entry-taxonomies"><div class="entry-categories"><span>Categories</span><ul class="post-categories">
+	<li><a href="https://pokemonfansub.alwaysdata.net/?cat=21" rel="category">Pokémon Horizons</a></li>
+	<li><a href="https://pokemonfansub.alwaysdata.net/?cat=6" rel="category">Release</a></li></ul></div></div>
+						<div class="post-navigation row">
+			<div class="col-md-6">
+								<span>Previous article</span>
+				<h2 class="h5"><a href="https://pokemonfansub.alwaysdata.net/?p=8335" rel="prev">Pokémon Horizons 119 et 120 VOSTFR</a></h2>
+							</div>
+			<div class="col-md-6 post-navi-next">
+							</div>
+		</div>
+		
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
+					
+<!-- <div id="comments" class="comments-area"> -->
+
+				
+				</main><!-- #main -->
+			</div><!-- #primary -->
