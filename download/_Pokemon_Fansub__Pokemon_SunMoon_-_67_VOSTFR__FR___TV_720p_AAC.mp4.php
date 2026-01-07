72952f21c8d2549e9c976183e09da56dd671851c
andykimpe1 <andykimpe@gmail.com>
2026-01-07 16:44:19 +0100

    Update _Pokemon_Fansub__Pokemon_SunMoon_-_67_VOSTFR__FR___TV_720p_AAC.mp4.php

--- download/_Pokemon_Fansub__Pokemon_SunMoon_-_67_VOSTFR__FR___TV_720p_AAC.mp4.php
+++ download/_Pokemon_Fansub__Pokemon_SunMoon_-_67_VOSTFR__FR___TV_720p_AAC.mp4.php
@@ -1,14 +1,11 @@
 <?php
 include "../function.php";
 $name = "_Pokemon_Fansub__Pokemon_SunMoon_-_67_VOSTFR__FR___TV_720p_AAC.mp4";
-$urlname = urlencode($name);
-$url = https://yourprivatehosting/torrent/debrid.php?name=" . $urlname . "";
-$user_agent = null;
-$hdrs = null;
+$url = https://yourprivatehosting/torrent/debrid.php?name=" . urlencode($name) . "";
 $post = http_build_query([
     'link' => "" . $link . "",
 ]);
-$cr = curl($url, $user_agent, "false", "false", $hdrs, $post);
+$cr = curl($url, null, "false", "false", null, $post);
 header("Location: " . $cr . "");
 exit;
 
