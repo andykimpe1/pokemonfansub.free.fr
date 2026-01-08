2291ca996961e62c12c5dfea68a15054595aca76
andykimpe1 <andykimpe@gmail.com>
2026-01-08 13:05:37 +0100

    Delete search.php

--- search.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (isset($_GET["search"])) {
-    $search = $_GET["search"];
-} else {
-	header("Location: http://pokemonfansub.alwaysdata.net/");
-    exit;
-}
-header("Location: https://www.google.com/search?client=ms-google-coop&q=site:pokemonfansub.alwaysdata.net+" . $search . "");
